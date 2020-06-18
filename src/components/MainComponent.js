fc6a8393480191f92c8b2a5bb9151625938ed3a4
bepul0 <bepul.cse.ru@gmail.com>
2020-06-18 11:52:33 +0600

    Presentational and Container Components

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
 
-import "./App.css";
-import Main from "./components/MainComponent";
-class App extends Component {
+import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./MenuComponent";
+import { DISHES } from "../shared/dishes";
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
   render() {
     return (
       <div>
-        <Main />
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
 }
-export default App;
+
+export default Main;
 
 // import React, { Component } from "react";
 // import logo from "./logo.svg";
